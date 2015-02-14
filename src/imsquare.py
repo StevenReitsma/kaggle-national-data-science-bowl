8bfff4406c173b07484529cb8adb3767ed77885f
Guido Zuidhof <me@guido.io>
2015-02-14 17:13:12 +0100

    Image squaring by padding done

--- src/imsquare.py
+++ src/imsquare.py
@@ -1,16 +1,63 @@
 from scipy import misc
+import numpy as np
 
 #a = misc.imread('../data/train/amphipods/4661.jpg')
+#print a
 
+
+# Square an image by padding its sides
 def squarepad(image, padvalue=255):
+    
+    height = len(image)
+    width = len(image[0])    
+    
+    #Desired width and height length.
+    desiredsize = max([height, width])
+    
+    
+    if width < desiredsize : # Pad to the left and right
+        
+        leftlength, rightlength = calcpadsize(width, desiredsize)
+        
+        
+        lpad = np.empty([height, leftlength], dtype=int)
+        rpad = np.empty([height, rightlength], dtype=int)          
+        
+        rpad.fill(padvalue)        
+        lpad.fill(padvalue)
+        
+        # Horizontally stack the paddings around the image
+        image = np.hstack((lpad, image, rpad))
+        
+    if height < desiredsize :  # Pad to the top and bottom
+       
+        
+        toplength, bottomlength = calcpadsize(height, desiredsize)
+
+        tpad = np.empty([toplength, width], dtype=int)
+        bpad = np.empty([bottomlength, width], dtype=int)
+
+        tpad.fill(padvalue)
+        bpad.fill(padvalue)
+
+        # Vertically stack the paddings around the image
+        image = np.vstack((tpad, image, bpad))        
+        
     return image
     
 
-# Load images from folder into memory
-#def loadImages(folder):
-#    pass
-    
-#square_pad() 
 
+# Returns the pad sizes for either side of the image
+def calcpadsize(currentlength, desiredlength):
+    padlength = desiredlength - currentlength
     
+    l = padlength // 2
+    r = padlength - l
+    
+    
+    return l, r
+    
+
+ 
+
 
--- test/test-im-square.py
+++ test/test-im-square.py
@@ -1,7 +1,9 @@
 import unittest
+import numpy as np
 import sys
 sys.path.append('../src')
 import imsquare as imsquare
+
 
 
 class TestImageSquaring(unittest.TestCase):
@@ -19,47 +21,88 @@ class TestImageSquaring(unittest.TestCase):
             [0,0,0]
         ]
         
+        self.verywiderect = [
+            [0,0,0,0,0],
+            [0,0,0,0,0]
+        ]
+        
         self.tallrect = [
             [0,0],
             [0,0],
             [0,0]
         ]
         
+        self.verytallrect = [
+            [0,0],
+            [0,0],
+            [0,0],
+            [0,0],
+            [0,0]
+        ]
         
         
     def test_pad_square_image(self):
         #Image is already square, output should not change
         
         squared = imsquare.squarepad(self.alreadySquare)
-        self.assertEqual(self.alreadySquare, squared)
+        expected = self.alreadySquare
+                
+        
+        self.assertTrue(np.array_equal(squared, expected))     
 
     def test_pad_wide_image(self):
-        # Pad non-square image with 1s
+        # Pad small non-square image with 1s
         squared = imsquare.squarepad(self.widerect, 1)
-        
         expected = [
             [0,0,0],
             [0,0,0],
             [1,1,1]
         ]      
         
-        self.assertEqual(squared, expected)        
+        self.assertTrue(np.array_equal(squared, expected))          
+        
+        # Test very wide image (requires padding to both top and bottom)
+        squared = imsquare.squarepad(self.verywiderect, 1)
+        expected = [
+            [1,1,1,1,1],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [1,1,1,1,1],
+            [1,1,1,1,1]
+        ]      
+        
+        self.assertTrue(np.array_equal(squared, expected))   
+        
+        
+        
+        
+        
         
     def test_pad_tall_image(self):
-        # Pad non-square image with 1s
+        # Pad small non-square image with 1s
         squared = imsquare.squarepad(self.tallrect, 1)
-        
         expected = [
             [0,0,1],
             [0,0,1],
             [0,0,1]
-        ]      
+        ]
         
-        self.assertEqual(squared, expected)     
+        self.assertTrue(np.array_equal(squared, expected))          
     
     
-
-
+        # Pad very tall image (requries padding to both left and right)
+        squared = imsquare.squarepad(self.verytallrect, 1)
+        expected = [
+            [1,0,0,1,1],
+            [1,0,0,1,1],
+            [1,0,0,1,1],
+            [1,0,0,1,1],
+            [1,0,0,1,1]
+        ]
+        
+        self.assertTrue(np.array_equal(squared, expected))  
+    
+    
 
 
 if __name__ == '__main__':
