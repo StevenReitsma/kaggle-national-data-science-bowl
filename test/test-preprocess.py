197a7b5878d7b8fd25fa3888c9cf046e725a7aa4
Guido Zuidhof <me@guido.io>
2015-02-24 23:32:00 +0100

    Mini process integration test

--- test/test-preprocess.py
+++ test/test-preprocess.py
@@ -4,12 +4,19 @@ import unittest
 import sys
 sys.path.append('../src')
 import preprocess
+import imsquare
+import numpy as np
 
 class TestPreprocess(unittest.TestCase):
     
     def setUp(self):
         # Testing variables setup
         self.classnames = ['a', 'b', 'c']
+        
+        self.image = np.array([
+            [0,1,2,3,4],
+            [5,6,7,8,9]
+        ])
         
     
     def test_generate_label_dict(self):
@@ -20,6 +27,26 @@ class TestPreprocess(unittest.TestCase):
         self.assertTrue(label_dict['b'] == 1)
         
         self.assertEqual(len(label_dict), 3)
+
+    #Mini integration test
+    def test_process_image(self):
+        patch_size = 8
+        resize_size = 64     
+        square_function = imsquare.square_pad 
+        
+        expected_patches_amount = (resize_size-patch_size+1)**2 # = 3249
+        
+        processed, patches = preprocess.process(self.image, 
+                                                square_function, 
+                                                patch_size, 
+                                                resize_size)
+        
+        self.assertEqual(expected_patches_amount, len(patches))
+        self.assertEqual( (64,64), np.shape(processed))
+        
+        for patch in patches:
+            #Patches should be onedimensional
+            self.assertEqual(patch_size**2, len(patch))
 
 if __name__ == '__main__':
     unittest.main()
