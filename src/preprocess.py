e91c3f62cc89f0b46cc42a40c60eeb5bc149607e
Guido Zuidhof <me@guido.io>
2015-02-24 22:29:39 +0100

    Preprocess refactor, using kwargs

--- src/preprocess.py
+++ src/preprocess.py
@@ -11,7 +11,6 @@ import imutil
 from scipy import misc
 import h5py
 
-
 """
 Preprocessing script
 
@@ -29,69 +28,75 @@ Preprocessing script
 """
 
 def preprocess(path='../data/train', 
-               outpath="../data/preprocessed.h5", patchsize=6, imagesize=32):
+               outpath="../data/preprocessed.h5", **kwargs):
     
     
-    print "Patch size: {0}x{0} = {1}".format(patchsize, patchsize*patchsize)
-    print "Image size: {0}".format(imagesize)
-    
+    patch_size = kwargs.get('patch_size', 6)
+    image_size = kwargs.get('image_size', 32)
+    square_method = kwargs.get('square_method', 'pad')
+        
+    square_function = imsquare.get_square_function_by_name(square_method)
     
     labels = get_image_paths(path) 
     n = len(labels)
     
-    print "Amount of images: {0}".format(n)
-    
-    
     f = h5py.File(outpath, 'w')
     
     # Calculate some dimensions
-    patches_per_image = impatch.npatch(imagesize, patchsize)
+    patches_per_image = impatch.npatch(image_size, patch_size)
     patches_total = n*patches_per_image
     
+    
+    print "Patch size: {0}x{0} = {1}".format(patch_size, patch_size**2)
+    print "Image size: {0}".format(image_size)
+    print "Square method: {0}".format(square_method)
+    print "Amount of images: {0}".format(n)
     print "Patches per image: {0}".format(patches_per_image)
     print "Patches total: {0}".format(patches_total)
     
     #Dimension of what will be written to file
-    dimallpatches = (patches_total, patchsize*patchsize)
+    dim_all_patches = (patches_total, patch_size**2)
     
     #Create dataset (in file)
-    dset = f.create_dataset('data', dimallpatches)
+    dset = f.create_dataset('data', dim_all_patches)
 
     #Write metadata to file (options)
     write_metadata(dset, 
-                   patch_size = patchsize, 
-                   image_size = imagesize, 
-                   patches_per_image=patches_per_image)
+                   patch_size, 
+                   image_size, 
+                   patches_per_image,
+                   square_method)
     
     
     for i, (classname, filename, filepath) in enumerate(labels):
         
         image = misc.imread(filepath)
-        image, patches = process(image, patchsize=patchsize, imagesize=imagesize)
+        image, patches = process(image, square_function, patch_size, image_size)
          
-        dset[i*patches_per_image:i*patches_per_image+len(patches)] = patches
+        start_index = i*patches_per_image
+        dset[start_index:start_index+len(patches)] = patches
         
         if i % 20 == 0:
             util.update_progress(i/n)
     
     f.close()
-    
     util.update_progress(1.0)
    
-def write_metadata(dataset, patch_size=6, image_size=32, patches_per_image=729):
+def write_metadata(dataset, patch_size, image_size, patches_per_image, square_method):
     dataset.attrs['patch_size'] = patch_size
     dataset.attrs['image_size'] = image_size
     dataset.attrs['patches_per_image'] = patches_per_image
+    dataset.attrs['square_method'] = square_method
 
-def process(image, squarefunction=imsquare.squarepad, patchsize=6, imagesize=32, ):
+def process(image, squarefunction, patch_size, image_size):
     """
         Process a single image (make square, resize, extract patches, flatten patches)
     """
     
     image = squarefunction(image)
-    image = imutil.resize_image(image, imagesize)
+    image = imutil.resize_image(image, image_size)
     
-    patches = impatch.patch(image, patchsize = patchsize)
+    patches = impatch.patch(image, patch_size = patch_size)
     patches = [imutil.flatten_image(patch) for patch in patches]
     return image, patches
     
@@ -109,6 +114,9 @@ def get_image_paths(path):
                 labels.append((classname, filename, filepath))
     
     return labels
+
+
+
 
 
 if __name__ == '__main__':
