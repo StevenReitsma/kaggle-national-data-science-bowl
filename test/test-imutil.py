7c407332fa99953680f6b826eba4e1506e6f7f7f
Guido Zuidhof <me@guido.io>
2015-02-24 21:12:23 +0100

    Image resize test

--- test/test-imutil.py
+++ test/test-imutil.py
@@ -28,6 +28,17 @@ class TestPreprocessing(unittest.TestCase):
         
         self.assertTrue(np.array_equal(flat, expected))         
         
+    def test_resize_images(self):
+        
+        im = self.images[0]
+        
+        resized = imutil.resize_image(im, 64)
+        
+        # Square
+        self.assertEqual(len(resized), len(resized[0]))
+        
+        # Right size
+        self.assertEqual(len(resized), 64)
 
 if __name__ == '__main__':
     unittest.main()
