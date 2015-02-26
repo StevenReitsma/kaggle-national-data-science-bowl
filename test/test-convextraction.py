2643aa42d647f28fcf2b7a56123c8377d6730772
robbertvdg <robbert.gugten@student.ru.nl>
2015-02-26 14:26:20 +0100

    Now reshapes one image

--- test/test-convextraction.py
+++ test/test-convextraction.py
@@ -18,20 +18,40 @@ class TestConvExtraction(unittest.TestCase):
             [1,2,3,4,5],
             [6,7,8,9,0],
             [0,9,8,7,6],
+            [5,4,3,2,1],
+            [1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
+            [5,4,3,2,1],
+            [1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
             [5,4,3,2,1]
            ])
 
-
+        self.result = np.array([
+            [[1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
+            [5,4,3,2,1]],
+            [[1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
+            [5,4,3,2,1]],
+            [[1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
+            [5,4,3,2,1]],
+            [[1,2,3,4,5],
+            [6,7,8,9,0],
+            [0,9,8,7,6],
+            [5,4,3,2,1]
+           ]])
 
 
     def test_conv(self):
-        self.test = convextraction.extract(self.testImage,2)
-        result = np.zeros((2,2,5))
-        result[0,0,:] = np.array([1,2,3,4,5])
-        result[0,1,:] = np.array([6,7,8,9,0])
-        result[1,0,:] = np.array([0,9,8,7,6])
-        result[1,1,:] = np.array([5,4,3,2,1])
-        self.assertEqual(self.test, result)
+        self.test = convextraction.extract(self.testImage,4)
+        self.assertEqual(self.test, self.result) #does not work on np matrices i think..
 
     def test_pool(self):
         pass
