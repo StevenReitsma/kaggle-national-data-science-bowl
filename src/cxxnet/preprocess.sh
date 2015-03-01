7dc293576b1fdd412e796f81ebe194c1afebb0fb
Steven Reitsma <steven@properchaos.nl>
2015-03-01 22:19:14 +0100

    Bugfix in directory structure.

--- src/cxxnet/preprocess.sh
+++ src/cxxnet/preprocess.sh
@@ -12,7 +12,7 @@ if [ "$1" == train ]; then
 	# Generate new data
 	python gen_train.py $DIR/../../data/train/ $DIR/../../data/cxxnet/train/
 	python gen_img_list.py train $DIR/../../data/sampleSubmission.csv $DIR/../../data/cxxnet/train/ $DIR/../../data/cxxnet/train.lst
-	$CXXNET_DIR/tools/im2bin $DIR/../../data/cxxnet/train.lst ./ $DIR/../../data/cxxnet/train.bin
+	$CXXNET_DIR/tools/im2bin $DIR/../../data/cxxnet/train.lst / $DIR/../../data/cxxnet/train.bin
 elif [ "$1" == test ]; then
 	# Cleanup
 	rm -rf $DIR/../../data/cxxnet/test/*
@@ -22,5 +22,5 @@ elif [ "$1" == test ]; then
 	# Generate new data
 	python gen_test.py $DIR/../../data/test/ $DIR/../../data/cxxnet/test/
 	python gen_img_list.py test $DIR/../../data/sampleSubmission.csv $DIR/../../data/cxxnet/test/ $DIR/../../data/cxxnet/test.lst
-	$CXXNET_DIR/tools/im2bin $DIR/../../data/cxxnet/test.lst ./ $DIR/../../data/cxxnet/test.bin
+	$CXXNET_DIR/tools/im2bin $DIR/../../data/cxxnet/test.lst / $DIR/../../data/cxxnet/test.bin
 fi
