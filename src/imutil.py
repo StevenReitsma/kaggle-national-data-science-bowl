a0d98169ad72da435f38c3c68caef0a3cb8f158c
Guido Zuidhof <me@guido.io>
2015-03-02 22:36:32 +0100

    Image sum util

--- src/imutil.py
+++ src/imutil.py
@@ -9,3 +9,6 @@ def flatten_image(image):
     
 def resize_image(image, size=32, interp='bilinear'):
     return ( misc.imresize(image, (size, size), interp))
+
+def sum_images(sequence_of_images):
+    return sum(sequence_of_images)
