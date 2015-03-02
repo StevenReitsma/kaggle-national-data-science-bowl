0e64b49ca0b465781b4785f9f34ffcb869780918
Guido Zuidhof <me@guido.io>
2015-03-02 23:22:03 +0100

    Plot mean image

--- src/util.py
+++ src/util.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import os
+import math
 
 def load_unsupervised(filepath="../data/preprocessed.h5", shuffle=True):
     f = h5py.File(filepath)
@@ -31,6 +32,14 @@ def normalize(images):
     return [image/float(255) for image in images]
     
     
+def plot_mean_image():
+    meta = load_metadata()
+    mean = meta['mean_image']
+    size = int(math.sqrt(len(mean)))
+    mean = np.reshape(mean, (size,size))
+    plt.imshow(mean)
+    
+
 def plot_centroids(centroids, file_path, im_size = (6,6)): 
     if not os.path.exists(file_path):
         os.makedirs(file_path)
