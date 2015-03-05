1c51679cc8589f50396b5e96d0ad260d51dcd749
robbertvdg <robbert.gugten@student.ru.nl>
2015-03-06 00:13:51 +0100

    Transformation added, todo find correct bias

--- src/RBMtrain.py
+++ src/RBMtrain.py
@@ -11,27 +11,42 @@ import RBM
 
 #from preprocess import preprocess #For using code from different branch
 
-###############################################################################
-# Setting up
+_hidden_units = 100
 
+#
 def RBMtraining():
     counter = 1
     for data in randombatchreader.RandomBatchReader():
     #for data in batchreader.BatchReader():
         if counter == 1:
-            rbm = RBM.RBM(len(data[0]), 100)
+            rbm = RBM.RBM(len(data[0]), _hidden_units)
         print ("FITTING CHUNK :" + str(counter))
-        rbm.train(data,max_epochs=5)
+        data2 = np.array([1-(x/float(255)) for x in data])
+    #    print(data)
+        rbm.train(data2,max_epochs=10)
         counter+=1
-
+        if counter == 3:
+            break
     print ("DONE")
     print(rbm.weights)
 
     return rbm
 
-def getWeights(rbm,data):
-    return rbm.run_visible(data)
+#Finds the hidden layer probability values belonging to data
+#TODO find the correct bias!!
+def transform(rbm,data):
+    X = []
+    for dat in data:
+        Y = np.zeros(_hidden_units)
+        for i in range(_hidden_units):
+            Y[i] = logistic(rbm.weights[0,i] + sum(rbm.weights[1:,i] * dat))
+#           print(rbm.weights[1:,i])
+#           print(dat)
+        X.append(Y)
+    return X
 
+def logistic(x):
+    return 1.0 / (1 + np.exp(-x))
 
 if __name__ == "__main__":
     #Probably load data here!
@@ -39,10 +54,11 @@ if __name__ == "__main__":
     rbm = RBMtraining()
     for data in batchreader.BatchReader():
     #    print(data)
-    #    X_train = [1-(x/float(255)) for x in data]
-        weights = getWeights(rbm,data)
+        X_train = [1-(x/float(255)) for x in data]
+        weights = transform(rbm,X_train)
+#       now pooling! TODO
         print (weights)
-        break
+#        break
 
 
 
