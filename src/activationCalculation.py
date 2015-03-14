2d7fd497db1897ba38e3a64f5168d70453c8aa7d
Guido Zuidhof <me@guido.io>
2015-03-14 15:54:51 +0100

    Plotting activations.

--- src/activationCalculation.py
+++ src/activationCalculation.py
@@ -12,6 +12,7 @@ import batchreader
 import os
 import h5py
 import util
+import matplotlib.pyplot as plt
 
 
 class ActivationCalculation():
@@ -19,10 +20,10 @@ class ActivationCalculation():
         
     def distance_to_centroids(self, patches, centroids):
         # Triangle (soft) activation function
-        pp = np.sum(np.square(patches), axis=1) # Dot product patchesxpatches
+        pp = np.sum(np.square(patches), axis=1) # Dot product patchesxpatches\
         cc = np.sum(np.square(centroids), axis=1) # Dot product centroidsxcentroids
         pc = 2*np.dot(patches, centroids.T) # 2* Dot product patchesxcentroids
-    
+
         z = np.sqrt(cc + (pp - pc.T).T) # Distance measure
         mu = z.mean(axis=0)
         activation = np.maximum(0, mu-z) # Similarity measure
@@ -59,10 +60,16 @@ class ActivationCalculation():
         
         for i, batch in enumerate(batches):
             activation = self.distance_to_centroids(batch, centroids) # Calculate activations for each patch to each centroid
+            
+            self.visualize_activation(activation)            
+            
+            
             activations[i] = pool(activation, n_pool_regions = n_pool_regions) # Returns a vector with length 4x#centroids
             util.update_progress(i/batches.nbatches)
             
         util.update_progress(1)
+        
+        
         print "Normalizing activations..."
         activations = self.normalize(activations)
         print "Normalizing done"
@@ -76,10 +83,32 @@ class ActivationCalculation():
         return activations
 
         
+        
+        
+    def visualize_activation(self, activations):
+        patch_size = np.sqrt(activations.shape[0])
+        n_features = activations.shape[1]
+    
+        # Reshape to 2D slabs
+        reshaped = np.reshape(activations.T, (n_features, patch_size, patch_size))
+        
+
+        length = int(np.sqrt(reshaped.shape[0]))
+        
+        f, ax = plt.subplots(length, length)
+        
+        for i in range(0, length):
+            for j in range(0, length):
+             ax[i, j].imshow(reshaped[i*length+j], cmap = 'Greys', interpolation = 'nearest')
+             ax[i, j].axis('off')
+        
+        plt.show()
+
     
 if __name__ == '__main__':
     km = kmeans.kMeansTrainer()
-    centroids = km.get_saved_centroids()
+    centroids = km.get_saved_centroids(100)
+    util.plot_centroids(centroids, "../data/centroidskmeans")
     sup_km = ActivationCalculation()
     sup_km.pipeline(centroids = centroids)
 
